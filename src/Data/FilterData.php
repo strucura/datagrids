8179ef238c40c5dd540a323d3864200537ce52c6
andyleach <andyleach@users.noreply.github.com>
2024-10-31 17:56:48 +0000

    Fix styling

--- src/Data/FilterData.php
+++ src/Data/FilterData.php
@@ -7,8 +7,8 @@ use Strucura\Grids\Enums\FilterTypeEnum;
 class FilterData
 {
     public function __construct(
-        public string         $column,
-        public mixed          $value,
+        public string $column,
+        public mixed $value,
         public FilterTypeEnum $filterType,
     ) {
         // Run through value transformers
