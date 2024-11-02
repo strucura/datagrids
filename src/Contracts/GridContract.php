ef45f18f7af8bc76ea9dae9a57aeb92ff7ab49df
Andrew Leach <7387639+andyleach@users.noreply.github.com>
2024-11-02 17:33:25 -0400

    Grid Registration

--- src/Contracts/GridContract.php
+++ src/Contracts/GridContract.php
@@ -3,10 +3,18 @@
 namespace Strucura\DataGrid\Contracts;
 
 use Illuminate\Database\Query\Builder;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Collection;
+use Strucura\DataGrid\Requests\GridDataRequest;
 
 interface GridContract
 {
+    public function getPermissionName(): string;
+    public function getRoutePath(): string;
+    public function getRouteName(): string;
+    public function handleData(GridDataRequest $request): JsonResponse;
+    public function handleSchema(): JsonResponse;
+
     public function getColumns(): Collection;
 
     public function getQuery(): Builder;
--- src/DataGridServiceProvider.php
+++ src/DataGridServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace Strucura\DataGrid;
 
+use Illuminate\Support\Facades\Route;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Spatie\StructureDiscoverer\Discover;
+use Strucura\DataGrid\Contracts\GridContract;
 
 class DataGridServiceProvider extends PackageServiceProvider
 {
@@ -17,5 +20,22 @@ class DataGridServiceProvider extends PackageServiceProvider
         $package
             ->name('datagrids')
             ->hasConfigFile();
+    }
+
+    public function packageRegistered()
+    {
+        $paths = config('datagrids.discovery.paths');
+        $conditions = config('datagrids.discovery.conditions');
+
+        $discoveredGridFQCNs = Discover::in(...$paths)
+            ->any(...$conditions)
+            ->get();
+
+        foreach ($discoveredGridFQCNs as $gridFQCN) {
+            /** @var GridContract $grid */
+            $grid = new $gridFQCN;
+            Route::post($grid->getRoutePath() .'/data', [$gridFQCN, 'handleData'])->name($grid->getRouteName().'.data');
+            Route::post($grid->getRoutePath() .'/schema', [$gridFQCN, 'handleSchema'])->name($grid->getRouteName().'.schema');
+        }
     }
 }
